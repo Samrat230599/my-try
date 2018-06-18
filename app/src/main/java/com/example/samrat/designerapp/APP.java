536ff45e444d15f448d2ac469f3113d8e161305d
samrat230599 <sergeikorolev1234@gmail.com>
2018-06-18 11:17:48 +0530

    Own try

---
+++ app/src/main/java/com/example/samrat/designerapp/APP.java
@@ -0,0 +1,13 @@
+package com.example.samrat.designerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class APP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+    }
+}
